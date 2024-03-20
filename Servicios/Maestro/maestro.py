ef578535595378f155e59991a401045d9731edc5
LucioCanovas <lucio.canovas@gmail.com>
2024-03-20 05:15:39 +0100

    Uso de schedule, contenerizado y metricas cpu_memoria

--- Servicios/Maestro/maestro.py
+++ Servicios/Maestro/maestro.py
@@ -1,6 +1,8 @@
 import sqlite3
 import docker
 import logging
+import schedule
+import psutil
 import time
 import telebot
 from datetime import datetime
@@ -11,7 +13,7 @@ def estado_servidor():
     contenedor.stop()
     contenedor.start()
 
-    conn = sqlite3.connect('/home/admincamaras/CamarasBiblioteca/Compartida/BBDD/BBDD.db')
+    conn = sqlite3.connect('/app/Compartida/BBDD/BBDD.db')
     cursor = conn.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS estado_servidor
@@ -28,9 +30,10 @@ def estado_servidor():
     conn.close()
     
 
-def comprobar_estado(estado_bib, detector_estado, bot, user_id):
+def comprobar_estado():
+    global estado_bib, detector_estado, bot, user_id
+
     client = docker.from_env()
-    contador_bib_cerrado = 0
     
     try:
         contenedor_detector = client.containers.get('detector')
@@ -38,36 +41,31 @@ def comprobar_estado(estado_bib, detector_estado, bot, user_id):
         for biblioteca, info_bib in estado_bib.items():
             estado = info_bib[0]
             nombre_contenedor_bib = info_bib[1]
-            contenedor_bib = client.containers.get(nombre_contenedor_bib)
-            horario = check_time_in_range(biblioteca)  # Supongo que tienes definida esta función
+            contenedor_bib = client.containers.get(info_bib[1])
+            horario = check_time_in_range(biblioteca)  # True, biblioteca debe de estar encendida, False biblioteca debe de estar apagada
             logging.debug(f'Argumentos: {estado_bib}, horario: {horario}')
 
-            if estado == 'Cerrado' and horario:
-                bot.send_message(user_id, f'Contenedor {nombre_contenedor_bib} iniciado')
-                detector_estado = True
-                if contenedor_detector.status != "running":
+            if estado == 'Cerrado' and horario: 
+                contenedor_bib.start()
+                bot.send_message(user_id, f'Contenedor detector encendido')
+                logging.info(f'El contenedor {nombre_contenedor_bib}, esta iniciado')
+                estado_bib[biblioteca] = ('Abierto', nombre_contenedor_bib)
+                
+                #Encendemos el detector si esta apagado
+                if not detector_estado:
+                    detector_estado = True
                     contenedor_detector.start()
+                    bot.send_message(user_id, f'Contenedor {nombre_contenedor_bib} encendido')
                     logging.info(f'El contenedor detector, esta iniciado')
-                    time.sleep(10)
-
-                if contenedor_bib.status != "running":
-                    contenedor_bib.start()
-                    logging.info(f'El contenedor {nombre_contenedor_bib}, esta iniciado')
-                    estado = 'Abierto'
-                    estado_bib[biblioteca] = (estado, nombre_contenedor_bib)
-
-            if estado == 'Abierto' and not horario:
-                if contenedor_bib.status == "running":
-                    logging.debug(f'El contenedor {nombre_contenedor_bib}, sigue en funcionamiento, pese a no estar en el horario')
-                else:
-                    estado_bib[biblioteca] = ('Cerrado', nombre_contenedor_bib)
-                    logging.info(f'El contenedor {nombre_contenedor_bib}, esta apagado y cerrado')
-                    bot.send_message(user_id, f'Contenedor {nombre_contenedor_bib} apagado')
-
-            if estado == 'Cerrado': contador_bib_cerrado += 1
 
 
-        if contador_bib_cerrado == len(estado_bib) and contenedor_detector.status != "running" and detector_estado:
+            if estado == 'Abierto' and not horario: #Los contenedores de cada biblioteca se deben de apagar por si solos 
+                estado_bib[biblioteca] = ('Cerrado', nombre_contenedor_bib)
+                logging.info(f'El contenedor {nombre_contenedor_bib}, esta cerrado')
+                bot.send_message(user_id, f'Contenedor {nombre_contenedor_bib} apagado')
+
+
+        if contenedor_detector.status != "running" and detector_estado:
             detector_estado = False
             contenedor_informes = client.containers.get('informes')
             contenedor_informes.start()
@@ -82,7 +80,7 @@ def comprobar_estado(estado_bib, detector_estado, bot, user_id):
 
 def check_time_in_range(codigo_bib):
     # Conectar a la base de datos SQLite3
-    conn = sqlite3.connect('/home/admincamaras/CamarasBiblioteca/Compartida/BBDD/BBDD.db')
+    conn = sqlite3.connect('/app/Compartida/BBDD/BBDD.db')
     cursor = conn.cursor()
     
     # Obtener la fecha actual en formato YYYY-MM-DD
@@ -131,7 +129,7 @@ def check_time_in_range(codigo_bib):
         # Cerrar la conexión a la base de datos
         conn.close()
 
-    logging.info(f'Horario: {hora_inicio} - {hora_fin}, Biblioteca: {codigo_bib}')
+    logging.debug(f'Horario: {hora_inicio} - {hora_fin}, Biblioteca: {codigo_bib}')
     if resultado:
         hora_inicio, hora_fin = resultado
         if hora_inicio == '00:00:00' and hora_fin == '00:00:00':
@@ -139,32 +137,55 @@ def check_time_in_range(codigo_bib):
         current_time = datetime.now().strftime('%H:%M:%S')
         return hora_inicio <= current_time <= hora_fin
     return False
-   
+
+
+def obtener_cpu_memoria():
+    global detector_estado
+    if detector_estado:
+        conn = sqlite3.connect("/app/Compartida/BBDD/BBDD.db")
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS memoria
+                        (timestamp TEXT, porcentaje REAL)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS cpu
+                        (timestamp TEXT, porcentaje REAL)''')
+        conn.commit()
+
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        porcentaje_cpu = psutil.cpu_percent()
+        memoria_info = psutil.virtual_memory()
+        porcentaje_memoria = memoria_info.percent
+
+        cursor.execute("INSERT INTO cpu (timestamp, porcentaje) VALUES (?, ?)", (timestamp, porcentaje_cpu))
+        cursor.execute("INSERT INTO memoria (timestamp, porcentaje) VALUES (?, ?)", (timestamp, porcentaje_memoria))
+
+        conn.commit()
+        conn.close()
+      
 
 bot = telebot.TeleBot("6679369644:AAHxfnQvlimJlF1ke7x_ewfFo7TuQwHI3Tc")
 user_id = '1849448005'
 
-  
-logging.basicConfig(filename='log_maestro.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info('Inicio de programa')
-cont = 0
+fecha_hoy = datetime.now().date()
 
-estado_bib = {'ANT': ('Abierto', 'camara_ant')}
-detector_estado = True
-t_inicio = time.time()
+# Convertir la fecha a formato YYYY-MM-DD
+fecha_formateada = fecha_hoy.strftime('%Y-%m-%d')
+
+ruta = f'/app/Compartida/logs/maestro/log_maestro_{fecha_formateada}.log'
+
+logging.basicConfig(filename=ruta, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info('Inicio de programa')
+
+estado_bib = {'ANT': ('Cerrado', 'camara_ant')}
+detector_estado = False
+
+schedule.every(1).hours.do(estado_servidor)
+schedule.every(5).minutes.do(comprobar_estado)
+schedule.every(5).seconds.do(obtener_cpu_memoria)
 
 while True:
-    t_comprobacion = time.time() - t_inicio
-    if t_comprobacion > 24 * 3600:
-        break
-    elif cont == 12:
-        cont = 0
-        estado_servidor()
+    # Verificar si hay tareas programadas para ejecutar
+    schedule.run_pending()
+    time.sleep(2)
+    
 
-    logging.debug('Comprobacion')
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    logging.debug(f'Estado Biblioteca (antes): {estado_bib}')
-    estado_bib, detector_estado = comprobar_estado(estado_bib, detector_estado, bot, user_id)
-    logging.debug(f'Estado Biblioteca(despues): {estado_bib}')
-    time.sleep(300)
-    cont+=1
+
--- Servicios/Maestro/p.py
+++ Servicios/Maestro/p.py
@@ -1,39 +1,16 @@
+import docker
 import time
 
-def comprobar_estado(estado_bib):
-    contador_bib_cerrado = 0
-    
-    try:
-
-        for biblioteca, info_bib in estado_bib.items():
-            estado = info_bib[0]
-            nombre_contenedor_bib = info_bib[1]
-            horario = True
-            print(f'Estado: {estado}, horario: {horario}')
-
-            if estado == 'Cerrado' and not horario:
-                contador_bib_cerrado += 1
-
-            elif estado == 'Cerrado' and horario:
-                estado_bib[biblioteca] = ('Abierto', nombre_contenedor_bib)
-
-
-            elif estado == 'Abierto' and not horario:
-                contador_bib_cerrado += 1
-                estado_bib[biblioteca] = ('Cerrado', nombre_contenedor_bib)
-
-            elif estado == 'Abierto' and horario:
-                continue
-
-        return estado_bib
-
-    except Exception as e:
-        # Manejar cualquier error de conexión con Docker u otro error
-        print(f"Error en comprobar_estado: {e}")
-
-estado_bib = {'ANT': ('Cerrado', 'camara_ant')}
+client = docker.from_env()
 
 while True:
-    estado_bib = comprobar_estado(estado_bib)
-    print(estado_bib)
-    time.sleep(15)
+    contenedor = client.containers.get('consumidor')
+    if contenedor.status == 'running':
+        print('Productor corriendo')
+        time.sleep(10)
+    else:
+        print('Productor detenido')
+        contenedor.start()
+        print('Productor iniciado')
+        time.sleep(10)
+
